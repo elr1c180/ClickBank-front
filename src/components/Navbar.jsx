40d2de74994f819a97c8f76d1c43bcc340ef331f
elr1c180 <elr1c.edw180@gmail.com>
2024-07-21 15:23:15 +0400

    first

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import cl from './myNavbar.module.css'
+
+const Navbar = () => {
+
+    return(
+        <div className={cl.myNavbar}>
+
+        </div>
+    )
+}
+
+export default Navbar;
