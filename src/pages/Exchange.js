7517903179f300b0571af1f14aa240152ab3e438
elr1c180 <elr1c.edw180@gmail.com>
2024-08-06 15:03:30 +0400

    mobile

--- src/pages/Exchange.js
+++ src/pages/Exchange.js
@@ -1,14 +1,35 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Card from "../components/Card/Card";
+import cl from './src/Exchange/Exchange.module.css'
+import qr from './src/Exchange/qr-code.png'
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Exchange = () => {
+    const [platform, setPlatform] = useState('');
+
+    useEffect(() => {
+        if (window.Telegram && window.Telegram.WebApp) {
+            const user = window.Telegram.WebApp.initDataUnsafe?.user;
+            if (user) {
+                setPlatform(window.Telegram.WebApp.platform)
+            }
+        }
+    }, []);
 
     return(
+        platform !== 'tdesktop' ? (
         <div>
             <Card />
             <Navbar/>
         </div>
+        )  : (
+            <div className={cl.MobileView}>
+            <h1>Available on Mobile only</h1>
+           <img src={qr} alt="mobileQr"/>
+             </div>
+        )
     )
 }
 
